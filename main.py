6dae44366f5d3fdecdb38807dd058405a3e080e7
JideMartins <jeedeyy@gmail.com>
2025-11-26 17:09:51 -0500

    Catch errors

--- main.py
+++ main.py
@@ -14,18 +14,22 @@ url = f"{GITHUB_API_URL}/users/{USERNAME}"
 
 # Get response
 response = requests.get(url)
+try:
+    # conditionals
+    if response.status_code == 200:
+        user_data = response.json() # returns dictionary
+        user_json = json.dumps(user_data, indent=4)
+        with open("user_data.json", "w") as f:
+            f.write(user_json)
+        
+    else:
+        print(f"Error fetching data: Status Code: {response.status_code}")
+except requests.exceptions.RequestException as e:
+    print(f"Network error occured; {e}")
+except json.JSONDecodeError as e:
+    print(f"Error parsing JSON response: {e}")
+except IOError as e:
+    print(f"Error writing to file: {e}")
 
-# conditionals
-if response.status_code == 200:
-    user_data = response.json() # returns dictionary
-    user_json = json.dumps(user_data, indent=4)
-    with open("user_data.json", "w") as f:
-        f.write(user_json)
-    # print(user_json)
-    # print(type(user_data))
-    # pprint(user_data)
-    
-else:
-    print(f"Error fetching data: Status Code: {response.status_code}")
 
 
