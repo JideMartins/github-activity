d6e1efc5d2ee4e248069419c8c0d1281695813a5
JideMartins <jeedeyy@gmail.com>
2025-11-25 21:33:09 -0500

    Load and dump json file

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
+import os
 import requests
+import json
 
 GITHUB_API_URL = "https://api.github.com/"
 username = "JideMartins"
+
+def convert_json(json_string):
+    return json.loads(json_string)
+
+def convert_dictionary(data_dict):
+    return json.dumps(data_dict, indent=4)
+
+def load_json(json_file):
+
+    if not os.path.exists(json_file):
+        # create file if it doesn't exists
+        with open(json_file, "w"):
+            pass
+    with open(json_file) as f:
+        json_string = f.read().strip()
+
+    if not json_string:
+        return "{}"
+    
+    return json_string
+
+def dump_json(json_file, data_json):
+    with open(json_file, "w") as f:
+        f.write(data_json)
+
 
 # State url here
 url = f"{GITHUB_API_URL}/users/{username}"
@@ -12,7 +39,8 @@ response = requests.get(url)
 # conditionals
 if response == 200:
     user_data = response.json()
-    print(user_data)
+    # print(user_data)
+    
 else:
     print(f"Error fetching data: Status Code: {response.status_code}")
     print(response.json())
