ce76bd3c6d83880708b725abd63642a98014e14e
JideMartins <jeedeyy@gmail.com>
2025-12-04 21:51:58 -0500

    Create and parse positional args

--- cli.py
+++ cli.py
@@ -3,3 +3,11 @@ from argparse import ArgumentParser
 # parser object
 parser = ArgumentParser(description="Simple CLI tool to display recent github activities")
 
+# positional argument
+parser.add_argument("username", type=str, help="Valid GitHub Username")
+
+# parse argument
+args = parser.parse_args()
+
+
+# Use argument
