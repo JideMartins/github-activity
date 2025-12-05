9795c67c83e0e367ee919e0f89a037d173fa8d8b
JideMartins <jeedeyy@gmail.com>
2025-12-05 01:46:13 -0500

    Update Version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="github-activity",
-    version="0.1.11",
+    version="0.1.12",
     py_modules=["cli", "github_events"],
     install_requires=[],
     entry_points={"console_scripts": ["gitact = cli:main"]},
