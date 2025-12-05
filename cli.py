f0784990c0ecc83fc48f060045710a46e8ae504f
JideMartins <jeedeyy@gmail.com>
2025-12-04 23:05:12 -0500

    Fix Positional argument (reqquired-True) - import and run get_events()

--- cli.py
+++ cli.py
@@ -1,13 +1,15 @@
 from argparse import ArgumentParser
+from github_events import get_events
 
 # parser object
 parser = ArgumentParser(description="Simple CLI tool to display recent github activities")
 
 # positional argument
-parser.add_argument("username", type=str, required=True, help="Valid GitHub Username")
+parser.add_argument("username", type=str, help="Valid GitHub Username")
 
 # parse argument
 args = parser.parse_args()
 
 
 # Use argument
+get_events(args.username)
--- github_events.py
+++ github_events.py
@@ -94,29 +94,5 @@ def get_events(username):
 
 
 
-# test
-get_events("octocat")
-
-# # endpoint url
-# url = f"{GITHUB_API_URL}/users/{USERNAME}"
-
-
-# # Get response
-# response = requests.get(url)
-# try:
-#     # conditionals
-#     if response.status_code == 200:
-#         user_data = response.json() # returns dictionary
-#         user_json = json.dumps(user_data, indent=4)
-#         with open("user_data.json", "w") as f:
-#             f.write(user_json)
-#             print("user data exported!")
-
-#     else:
-#         print(f"Error fetching data: Status Code: {response.status_code}")
-# except requests.exceptions.RequestException as e:
-#     print(f"Network error occured; {e}")
-# except json.JSONDecodeError as e:
-#     print(f"Error parsing JSON response: {e}")
-# except IOError as e:
-#     print(f"Error writing to file: {e}")
+# # test
+# get_events("octocat")
